66385cb55faaa0abbcc04093745bb7cb817db5b6
Pasi Ahopelto <pahopelt@gmail.com>
2017-03-15 19:22:28 +0200

    wicket-stuff: responsive styles

--- wicket-stuff/src/main/java/github/pasiahopelto/wicket/experiments/GridHome.java
+++ wicket-stuff/src/main/java/github/pasiahopelto/wicket/experiments/GridHome.java
@@ -1,7 +1,7 @@
 package github.pasiahopelto.wicket.experiments;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.ExternalImage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class GridHome extends WebPage {
@@ -10,11 +10,11 @@ public class GridHome extends WebPage {
 
 	public GridHome(final PageParameters parameters) {
 		super(parameters);
-		add(new Label("topNarrow", "Top Narrow"));
-		add(new Label("nestedNarrow", "Nested Narrow"));
-		add(new Label("nestedMedium", "Nested Medium"));
-		add(new Label("nestedWide", "Nested Wide"));
-		add(new Label("topWide", "Top Wide"));
+		add(new ExternalImage("topNarrow", "http://placehold.it/128x64"));
+		add(new ExternalImage("nestedNarrow", "http://placehold.it/64x64"));
+		add(new ExternalImage("nestedMedium", "http://placehold.it/128x64"));
+		add(new ExternalImage("nestedWide", "http://placehold.it/192x64"));
+		add(new ExternalImage("topWide", "http://placehold.it/384x64"));
 	}
 
 }
